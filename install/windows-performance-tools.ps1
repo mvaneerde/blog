f60d68b2bc743103302338ae2c1194ab86270d56
Matthew van Eerde <mvaneerde@hotmail.com>
2015-12-02 02:56:10 -0800

    Add windows performance toolkit

---
+++ install/windows-performance-tools.ps1
@@ -0,0 +1,25 @@
+# download and install the Windows Performance Toolkit
+
+$wpa = ${env:ProgramFiles(x86)} + "\Windows Kits\10\Windows Performance Toolkit\wpa.exe";
+
+if (Test-Path $wpa) {
+	$version = (Get-Item $wpa).VersionInfo.ProductVersion;
+
+	if ([version]$version -ge "10.0.10586.15") {
+		# already installed
+		Exit;
+	}
+}
+
+$setup = $env:temp + "\sdksetup.exe";
+
+if (!(Test-Path $setup)) {
+	# the Windows Performance Toolkit is in both the Windows SDK and the Windows ADK
+	# this particular link is for the SDK
+	$uri = "https://go.microsoft.com/fwlink/p/?LinkID=698771";
+	Invoke-WebRequest -Uri $uri -OutFile $setup;
+}
+
+& $setup /quiet /norestart /features OptionId.WindowsPerformanceToolkit | echo "Installing...";
+
+Remove-Item $setup;
