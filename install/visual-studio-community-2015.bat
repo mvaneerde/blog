039a62d89ee9af40faa4bb8781d1eb9c7f051118
Matthew van Eerde <mvaneerde@hotmail.com>
2016-01-12 13:42:39 -0800

    Add "force" option to Visual Studio install

--- install/visual-studio-community-2015.bat
+++ install/visual-studio-community-2015.bat
@@ -1,7 +1,13 @@
 @echo off
 setlocal
 
-if not exist "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0" (
-	echo Installing Visual Studio 2015...
-	powershell.exe -executionpolicy bypass %~dp0visual-studio-community-2015.ps1
+rem skip if we've already installed
+rem unless "force" is passed as an argument
+if /i not "%1" == "force" (
+	if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0" goto END
 )
+
+echo Installing Visual Studio 2015...
+powershell.exe -executionpolicy bypass %~dp0visual-studio-community-2015.ps1
+
+:END
