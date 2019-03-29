808ca6ceb43a0a4366d8d719f797f86054b15c89
Matthew van Eerde (^_^) <mateer@microsoft.com>
2019-03-29 16:13:03 -0700

    Remove devenum.exe dependency on DirectX SDK, retarget, check in built binary

--- devenum/devenum/devenum.cpp
+++ devenum/devenum/devenum.cpp
@@ -3,6 +3,14 @@
 #include <windows.h>
 #include <objbase.h>
 #include <stdio.h>
+
+// hack to avoid needing to install the DirectX SDK
+#pragma include_alias( "dxtrans.h", "qedit.h" )
+#define __IDxtCompositor_INTERFACE_DEFINED__
+#define __IDxtAlphaSetter_INTERFACE_DEFINED__
+#define __IDxtJpeg_INTERFACE_DEFINED__
+#define __IDxtKey_INTERFACE_DEFINED__
+
 #include <dshow.h>
 #include <ks.h>
 #include <ksmedia.h>
