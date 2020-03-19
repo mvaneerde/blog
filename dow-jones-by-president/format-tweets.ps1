d1ebf43d5df710847f7de2efb4b391e5a8fe1cc1
Matthew van Eerde <mvaneerde@gmail.com>
2020-03-19 12:25:58 -0700

    Create .csv file for tweets

---
+++ dow-jones-by-president/format-tweets.ps1
@@ -0,0 +1,20 @@
+Write-Output "<ul>"
+
+Import-Csv ".\tweets.csv" | ForEach-Object {
+    $row = $_;
+    
+    $htmlDate = [System.Net.WebUtility]::HtmlEncode((Get-Date ($row.Date)).ToString("yyyy-MM-dd"));
+    $htmlLink = [System.Net.WebUtility]::HtmlEncode($row.Link);
+    $htmlTweet = [System.Net.WebUtility]::HtmlEncode($row.Tweet);
+    $html = "${htmlDate}: <a href=`"${htmlLink}`">${htmlTweet}</a>";
+
+    If ($row.Comment)
+    {
+        $htmlComment = [System.Net.WebUtility]::HtmlEncode($row.Comment);
+        $html += " (${htmlComment})";
+    }
+
+    Write-Output "<li>$html</li>";
+}
+
+Write-Output "</ul>"
