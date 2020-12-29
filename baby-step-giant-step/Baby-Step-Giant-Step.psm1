b18328edf02137144b6cc1768479d9c3e721b320
Matthew van Eerde (^_^) <mateer@microsoft.com>
2020-12-29 06:21:33 -0800

    Extend baby-step giant-step to allow finding the logarithm of an element whose order is known to be a factor of (p - 1)

--- baby-step-giant-step/Baby-Step-Giant-Step.psm1
+++ baby-step-giant-step/Baby-Step-Giant-Step.psm1
@@ -39,20 +39,32 @@ Function Get-CeilingSquareRoot {
 
     Return $U;
 }
+Export-ModuleMember -Function Get-CeilingSquareRoot;
 
 # Calculates all the baby steps and returns them in a hash table
+# Optionally only calculate baby steps up to ceil(sqrt(q))
+# Where q is a prime factor of (p - 1)
 Function Get-BabySteps {
     Param(
         [Parameter(Mandatory)][bigint]$p,
-        [Parameter(Mandatory)][bigint]$g
+        [Parameter(Mandatory)][bigint]$g,
+        [bigint]$q
     )
 
-    $sqrt = Get-CeilingSquareRoot -n $p;
+    If ($q) {
+        $sqrt = Get-CeilingSquareRoot -n $q;
+    } Else {
+        $sqrt = Get-CeilingSquareRoot -n $p;
+    }
     $steps = @{};
 
     $i = [bigint]0;
     $g_i = [bigint]1;
     While ($i -le $sqrt) {
+        If ($steps.ContainsKey($g_i)) {
+            Break;
+        }
+
         $steps[$g_i] = $i;
         $i = $i + 1;
         $g_i = ($g_i * $g) % $p;
@@ -62,16 +74,30 @@ Function Get-BabySteps {
 }
 Export-ModuleMember -Function Get-BabySteps;
 
+# Given g, x, and p
+# Calculates the unique y such that g^y = x mod p
+# We can write this as y = log_g x mod p
+#
+# Optionally takes a precomputed list of baby steps
+# This is useful for taking the logs of several different x with the same g and p
+#
+# If g is known to have an orbit q which is smaller than (p - 1),
+# optionally takes the orbit q
 Function Get-Log {
     Param(
         [Parameter(Mandatory)][bigint]$p,
         [Parameter(Mandatory)][bigint]$g,
         [Parameter(Mandatory)][bigint]$x,
+        [bigint]$q,
         [hashtable]$babySteps
     )
 
+    If (!$q) {
+        $q = $p;
+    }
+
     If (!$babySteps) {
-        $babySteps = Get-BabySteps -p $p -g $g;
+        $babySteps = Get-BabySteps -p $p -g $g -q $q;
     }
 
     If ($babySteps.ContainsKey($x)) {
@@ -81,7 +107,7 @@ Function Get-Log {
     # We need to take at least one giant step
     # So we need s = ceil(sqrt(n))
     # and g^(-s)
-    $s = Get-CeilingSquareRoot -n $p;
+    $s = Get-CeilingSquareRoot -n $q;
     $g_negative_s = [bigint]::ModPow($g, $p - 1 - $s, $p);
 
     $giantSteps = [bigint]0;
@@ -89,6 +115,10 @@ Function Get-Log {
         # take a giant step
         $x = ($g_negative_s * $x) % $p;
         $giantSteps = $giantSteps + 1;
+
+        If ($giantSteps -gt $s) {
+            Throw "Took $giantSteps giant steps but baby steps still does not contain $x";
+        }
     }
 
     Return (($s * $giantSteps) + $babySteps[$x]) % $p;
