3305f0f15d1e23540c12e77a65e0517fb170e585
Matthew van Eerde <mvaneerde@gmail.com>
2021-04-24 19:34:19 -0700

    Add IMDB link

--- disney/format-data.ps1
+++ disney/format-data.ps1
@@ -11,9 +11,20 @@ $ps | ForEach-Object {
     $p = $_;
     $htmlMovie = [System.Net.WebUtility]::HtmlEncode($p.Movie);
     $htmlYear = [System.Net.WebUtility]::HtmlEncode($p.Year);
+
+    # A DIGRESSION ON ENCODING LINKS
+    # http://example.com/?x=a&y=b
+    # we want to put this into <a href="...">...</a>
+    # can we just do <a href="http://example.com/?x=a&y=b">...</a>? NO because that breaks things like &quot;
+    # instead we have to do <a href="http://example.com/?x=a&amp;y=b">...</a>
+    $htmlLink = [System.Net.WebUtility]::HtmlEncode($p.Link);
+
     $htmlCharacter = [System.Net.WebUtility]::HtmlEncode($p.Character);
 
-    Write-Output "<tr><td>$htmlMovie ($htmlYear)</td><td>$htmlCharacter</td></tr>";
+    Write-Output "<tr>";
+    Write-Output "<td><a href="$htmlLink">$htmlMovie ($htmlYear)</a></td>";
+    Write-Output "<td>$htmlCharacter</td>";
+    Write-Output "</tr>";
 };
 
 # Write footer
