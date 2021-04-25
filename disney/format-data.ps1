c5176c03d824ab70023c41a7cde211bb5f1edba3
Matthew van Eerde <mvaneerde@gmail.com>
2021-04-24 19:09:50 -0700

    Update Disney Princess data format script to actually do something

--- disney/format-data.ps1
+++ disney/format-data.ps1
@@ -1,1 +1,22 @@
-Write-Host "TODO";
+# Write header
+Write-Output "<table>";
+Write-Output "<thead><tr><th>Movie</th><th>Character</th></tr></thead>";
+Write-Output "<tbody>";
+
+# Load list of princesses
+$ps = Import-Csv ".\disney-princesses.csv";
+
+# Write a row for each princess
+$ps | ForEach-Object {
+    $p = $_;
+    $movie = $p.Movie;
+    $year = $p.Year;
+    $character = $p.Character;
+
+    # HACK HACK HACK - fix HTML encoding
+    Write-Output "<tr><td>$movie ($year)</td><td>$character</td></tr>";
+};
+
+# Write footer
+Write-Output "</tbody>";
+Write-Output "</table>"
