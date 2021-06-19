caf080fbd47ce3ef75cff9663bff58fd733cfeb5
Matthew van Eerde (HE/HIM) <mateer@microsoft.com>
2021-06-19 11:55:37 -0700

    Templatize Disney princess HTML generation script

--- disney/format-data.ps1
+++ disney/format-data.ps1
@@ -1,7 +1,24 @@
+Function Write-Html {
+    Param(
+        [string]$file,
+        [hashtable]$parameters
+    )
+
+    $html = Get-Content -Path $file -Raw;
+
+    If ($parameters -ne $null) {
+        $parameters.Keys | ForEach-Object {
+            $key = $_;
+            $value = $parameters[$key];
+            $html = $html.Replace("#" + $key + "#", [System.Net.WebUtility]::HtmlEncode($value));
+        }
+    }
+
+    Write-Output $html;
+}
+
 # Write header
-Write-Output "<table>";
-Write-Output "<thead><tr><th>Movie</th><th>Character</th></tr></thead>";
-Write-Output "<tbody>";
+Write-Html -file "header.txt";
 
 # Load list of princesses
 $ps = Import-Csv ".\disney-princesses.csv";
@@ -9,24 +26,14 @@ $ps = Import-Csv ".\disney-princesses.csv";
 # Write a row for each princess
 $ps | ForEach-Object {
     $p = $_;
-    $htmlMovie = [System.Net.WebUtility]::HtmlEncode($p.Movie);
-    $htmlYear = [System.Net.WebUtility]::HtmlEncode($p.Year);
 
-    # A DIGRESSION ON ENCODING LINKS
-    # http://example.com/?x=a&y=b
-    # we want to put this into <a href="...">...</a>
-    # can we just do <a href="http://example.com/?x=a&y=b">...</a>? NO because that breaks things like &quot;
-    # instead we have to do <a href="http://example.com/?x=a&amp;y=b">...</a>
-    $htmlLink = [System.Net.WebUtility]::HtmlEncode($p.Link);
-
-    $htmlCharacter = [System.Net.WebUtility]::HtmlEncode($p.Character);
-
-    Write-Output "<tr>";
-    Write-Output "<td><a href="$htmlLink">$htmlMovie ($htmlYear)</a></td>";
-    Write-Output "<td>$htmlCharacter</td>";
-    Write-Output "</tr>";
+    Write-Html -file "princess.txt" -parameters @{
+        "CHARACTER" = $p.Character;
+        "LINK" = $p.Link;
+        "MOVIE" = $p.Movie;
+        "YEAR" = $p.Year;
+    };
 };
 
 # Write footer
-Write-Output "</tbody>";
-Write-Output "</table>"
+Write-Html -file "footer.txt";
