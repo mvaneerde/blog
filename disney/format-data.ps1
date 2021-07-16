0518ceed3b33896a0afe0d6abb96ee939ab18aa8
Matthew van Eerde (^_^) <mateer@microsoft.com>
2021-07-16 08:04:17 -0700

    Add character-specific link to Disney princess database

--- disney/format-data.ps1
+++ disney/format-data.ps1
@@ -1,16 +1,25 @@
 Function Write-Html {
     Param(
         [string]$file,
-        [hashtable]$parameters
+        [hashtable]$textParameters,
+        [hashtable]$htmlParameters
     )
 
     $html = Get-Content -Path $file -Raw;
 
-    If ($parameters -ne $null) {
-        $parameters.Keys | ForEach-Object {
+    If ($textParameters -ne $null) {
+        $textParameters.Keys | ForEach-Object {
             $key = $_;
-            $value = $parameters[$key];
+            $value = $textParameters[$key];
             $html = $html.Replace("#" + $key + "#", [System.Net.WebUtility]::HtmlEncode($value));
+        }
+    }
+
+    If ($htmlParameters -ne $null) {
+        $htmlParameters.Keys | ForEach-Object {
+            $key = $_;
+            $value = $htmlParameters[$key];
+            $html = $html.Replace("#" + $key + "#", $value);
         }
     }
 
@@ -34,8 +43,8 @@ $ps | ForEach-Object {
             Write-Html -file "movie-footer.txt";
         }
 
-        Write-Html -file "movie-header.txt" -parameters @{
-            "LINK" = $p.Link;
+        Write-Html -file "movie-header.txt" -textParameters @{
+            "MOVIE_LINK" = $p."Movie Link";
             "MOVIE" = $p.Movie;
             "YEAR" = $p.Year;
         };
@@ -49,14 +58,32 @@ $ps | ForEach-Object {
         "Yes" = "Disney Princess";
     };
 
-    Write-Html -file "princess.txt" -parameters @{
-        "CHARACTER" = $p.Character;
+    $textParameters = @{
         "COMMENT" = $p.Comment;
         "HEIGHT" = $p.Height;
         "IMAGE" = $p.Image;
         "PRINCESS" = $princessPretty[$p.Princess];
         "WIDTH" = $p.Width;
     };
+
+    If ($p."Character Link") {
+        $characterHtml =
+            "<a href=`"" +
+            [System.Net.WebUtility]::HtmlEncode($p."Character Link") +
+            "`">" +
+            [System.Net.WebUtility]::HtmlEncode($p.Character) +
+            "</a>";
+    } Else {
+        $characterHtml = [System.Net.WebUtility]::HtmlEncode($p.Character);
+    }
+
+    $htmlParameters = @{
+        "CHARACTER_HTML" = $characterHtml;
+    }
+
+    Write-Html -file "princess.txt" `
+        -textParameters $textParameters `
+        -htmlParameters $htmlParameters;
 };
 
 If ($movie -ne "") {
