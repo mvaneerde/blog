ff4ccb36dfcbec99be603eaa21256a6cb710e9a7
Matthew van Eerde <mvaneerde@gmail.com>
2022-12-21 10:05:14 -0800

    Add position history and undo option to game play script

--- chess/play-game.ps1
+++ chess/play-game.ps1
@@ -9,14 +9,31 @@ If ($starting_position -eq "") {
 }
 Show-ChessPosition -fen $position;
 
+$history = [System.Collections.ArrayList]@();
+
 While ($true) {
-    $move = Read-Host "Enter a move in Universal Chess Interface (UCI) notation, or q to quit";
+    $can_undo = $history.Count -gt 0;
+    If ($can_undo) {
+        $prompt = "Enter a move in Universal Chess Interface (UCI) notation, u to undo, or q to quit";
+    } Else {
+        $prompt = "Enter a move in Universal Chess Interface (UCI) notation, or q to quit";
+    }
+
+    $move = Read-Host $prompt;
     
     If ($move -eq "q") {
         Break;
-    }
+    } ElseIf ($can_undo -and ($move -eq "u")) {
+        $position = $history[$history.Count - 1];
+        $history.RemoveAt($history.Count - 1);
 
-    Write-Host "";
-    $position = Add-MoveToChessPosition -fen $position -move $move;
-    Show-ChessPosition -fen $position;
+        Write-Host "Undid last move";
+        Show-ChessPosition -fen $position;
+    } Else {
+        $history.Add($position);
+
+        Write-Host "";
+        $position = Add-MoveToChessPosition -fen $position -move $move;
+        Show-ChessPosition -fen $position;
+    }
 }
