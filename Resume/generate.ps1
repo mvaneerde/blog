b0a7dc7b51a15eed2a1b84b501dc913200c937ac
Matthew van Eerde <mvaneerde@gmail.com>
2024-01-17 13:42:43 -0800

    Collect related variables

--- Resume/generate.ps1
+++ Resume/generate.ps1
@@ -1,13 +1,33 @@
 $variables = Get-Content -Path ".\data\variables.json" | ConvertFrom-Json;
 $template = Get-Content -Path ".\template\resume.md";
 
+$replacements = @{};
+
 $variables | Get-Member -MemberType "NoteProperty" | ForEach-Object {
+    $name = $_.Name;
+    $value = $variables.$name;
+
+    $key = $name.ToUpperInvariant();
+    If ($value -is [PSCustomObject]) {
+        $value | Get-Member -MemberType "NoteProperty" | ForEach-Object {
+            $name = $_.Name;
+            $value2 = $value.$name;
+
+            $key2 = $key + "-" + $name.ToUpperInvariant();
+            $replacements.$key2 = $value2;
+        }
+    } Else {
+        $replacements.$key = $value;
+    }
+}
+
+$replacements.GetEnumerator() | ForEach-Object {
     $key = $_.Name;
-    $value = $variables.$key;
+    $value = $_.Value;
 
-    $replace = "=" + $key.ToUpperInvariant().Replace(" ", "-") + "=";
+    $key = "=" + $key + "=";
 
-    $template = $template.Replace($replace, $value);
+    $template = $template.Replace($key, $value);
 }
 
 $template | Out-File -FilePath ".\matthew-van-eerde.md";
--- Resume/template/resume.md
+++ Resume/template/resume.md
@@ -1,5 +1,8 @@
 # =NAME= #
 
-=PHONE= - =EMAIL= - [=BLOG-DISPLAY=](=BLOG-LINK=)
+=PHONE= -
+=EMAIL= -
+[=BLOG-DISPLAY=](=BLOG-LINK=) -
+[=GITHUB-DISPLAY=](=GITHUB-NAME=)
 
 TODO - rest of resume
