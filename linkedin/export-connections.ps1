c66f4a8fd74e8d87f81b7ba57e8354e0f7f01653
Matthew van Eerde <mvaneerde@gmail.com>
2025-08-21 13:44:58 -0700

    Skip documentation comment header on connections.csv

--- linkedin/export-connections.ps1
+++ linkedin/export-connections.ps1
@@ -8,6 +8,7 @@ $archive = Get-Item -Path $zip;
 $expanded = "{0}\{1}" -f $archive.Directory.FullName, $archive.BaseName;
 
 If (!(Test-Path -Path $expanded -PathType Container)) {
+    Write-Host "Expanding $archive to $expanded";
     Expand-Archive -Path $archive -DestinationPath $expanded;
 }
 
@@ -15,4 +16,10 @@ If (!(Test-Path -Path $expanded -PathType Container)) {
 # Connections.csv - established connections (1st degree network)
 # Invitations.csv - pending incoming and outgoing connection requests
 # messages.csv - direct messages
+$connectionsPath = "{0}\{1}" -f $expanded, "Connections.csv";
+
+# the first three lines of Connections.csv are instructions, skip them
+$connections = Get-Content $connectionsPath | Select-Object -Skip 3 | ConvertFrom-Csv;
+$connections | Format-Table | Out-Host;
+
 Throw "TODO: process $expanded";
